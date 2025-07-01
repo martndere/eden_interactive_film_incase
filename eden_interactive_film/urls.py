5bd2d35b3adc7a0623d8117b8ae3b314110565a7
martndere <muruthindere78@gmail.com>
2025-07-01 03:05:23 -0400

    changes

--- eden_interactive_film/urls.py
+++ eden_interactive_film/urls.py
@@ -6,7 +6,7 @@ from rest_framework.routers import DefaultRouter
 from eden.views import ClipViewSet  # Update this to your app name!
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib.auth import views as auth_views
-from user_content import views as user_content_views
+from user import views as user_views
 
 router = DefaultRouter()
 router.register(r'clips', ClipViewSet)
@@ -18,7 +18,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('user/', include('user.urls', namespace='user')),
-    path('register/', user_content_views.register, name='register'),
+    path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='user_content/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
